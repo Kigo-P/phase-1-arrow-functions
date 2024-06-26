eb6b6001d4f06475f0b31af541fce3b079c0789e
Kigo-P <peterkigo33@gmail.com>
2024-06-26 18:40:17 +0300

    last commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function (){
+    let a = 2000;
+    let b = 100;
+    {return (a/b)};
+}
+console.log(divide ());
+
+let x = 2;
+const square = x => {return x ** 2}
+console.log(square(x));
+
+let a = 3;
+let b = 4
+const add = (a,b) => {return a + b}
+console.log(add(a,b));
